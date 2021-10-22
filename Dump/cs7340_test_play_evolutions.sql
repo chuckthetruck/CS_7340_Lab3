763f3bdcfdb0997fa1e445bc9adc64cce2260ab1
chuckthetruck <62222256+chuckthetruck@users.noreply.github.com>
2021-10-22 11:59:14 -0500

    Fixed schema

--- Dump/cs7340_test_play_evolutions.sql
+++ Dump/cs7340_test_play_evolutions.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.20, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
 --
 -- Host: localhost    Database: cs7340_test
 -- ------------------------------------------------------
--- Server version	8.0.20
+-- Server version	8.0.26
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -40,7 +40,7 @@ CREATE TABLE `play_evolutions` (
 
 LOCK TABLES `play_evolutions` WRITE;
 /*!40000 ALTER TABLE `play_evolutions` DISABLE KEYS */;
-INSERT INTO `play_evolutions` VALUES (1,'9ef3058d659d8f8f08b10b6c5c61fc9d822eaec2','2021-10-02 01:16:06','create table authors (\nauthor_id                     bigint auto_increment not null,\nauthor_name                   varchar(255),\nconstraint pk_authors primary key (author_id)\n);\n\ncreate table papers (\npaper_id                      bigint auto_increment not null,\ntitle                         varchar(255),\nbook_title                    varchar(255),\npages                         varchar(255),\nyear                          varchar(255),\nurl                           varchar(255),\nee                            varchar(255),\npublisher                     varchar(255),\njournal                       varchar(255),\nvolume                        varchar(255),\nnumber                        varchar(255),\ncrossref                      varchar(255),\nisbn                          varchar(255),\nconstraint pk_papers primary key (paper_id)\n);\n\ncreate table paperstoauthors (\npaper_id                      bigint not null,\nauthor_id                     bigint not null,\nconstraint pk_paperstoauthors primary key (paper_id,author_id)\n);\n\nalter table paperstoauthors add constraint fk_paperstoauthors_papers foreign key (paper_id) references papers (paper_id) on delete restrict on update restrict;\ncreate index ix_paperstoauthors_papers on paperstoauthors (paper_id);\n\nalter table paperstoauthors add constraint fk_paperstoauthors_authors foreign key (author_id) references authors (author_id) on delete restrict on update restrict;\ncreate index ix_paperstoauthors_authors on paperstoauthors (author_id);','alter table paperstoauthors drop foreign key fk_paperstoauthors_papers;\ndrop index ix_paperstoauthors_papers on paperstoauthors;\n\nalter table paperstoauthors drop foreign key fk_paperstoauthors_authors;\ndrop index ix_paperstoauthors_authors on paperstoauthors;\n\ndrop table if exists authors;\n\ndrop table if exists papers;\n\ndrop table if exists paperstoauthors;','applied','');
+INSERT INTO `play_evolutions` VALUES (1,'9ef3058d659d8f8f08b10b6c5c61fc9d822eaec2','2021-10-06 21:07:56','create table authors (\nauthor_id                     bigint auto_increment not null,\nauthor_name                   varchar(255),\nconstraint pk_authors primary key (author_id)\n);\n\ncreate table papers (\npaper_id                      bigint auto_increment not null,\ntitle                         varchar(255),\nbook_title                    varchar(255),\npages                         varchar(255),\nyear                          varchar(255),\nurl                           varchar(255),\nee                            varchar(255),\npublisher                     varchar(255),\njournal                       varchar(255),\nvolume                        varchar(255),\nnumber                        varchar(255),\ncrossref                      varchar(255),\nisbn                          varchar(255),\nconstraint pk_papers primary key (paper_id)\n);\n\ncreate table paperstoauthors (\npaper_id                      bigint not null,\nauthor_id                     bigint not null,\nconstraint pk_paperstoauthors primary key (paper_id,author_id)\n);\n\nalter table paperstoauthors add constraint fk_paperstoauthors_papers foreign key (paper_id) references papers (paper_id) on delete restrict on update restrict;\ncreate index ix_paperstoauthors_papers on paperstoauthors (paper_id);\n\nalter table paperstoauthors add constraint fk_paperstoauthors_authors foreign key (author_id) references authors (author_id) on delete restrict on update restrict;\ncreate index ix_paperstoauthors_authors on paperstoauthors (author_id);','alter table paperstoauthors drop foreign key fk_paperstoauthors_papers;\ndrop index ix_paperstoauthors_papers on paperstoauthors;\n\nalter table paperstoauthors drop foreign key fk_paperstoauthors_authors;\ndrop index ix_paperstoauthors_authors on paperstoauthors;\n\ndrop table if exists authors;\n\ndrop table if exists papers;\n\ndrop table if exists paperstoauthors;','applied','Table \'authors\' already exists [ERROR:1050, SQLSTATE:42S01]');
 /*!40000 ALTER TABLE `play_evolutions` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -53,4 +53,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-10-20 14:11:38
+-- Dump completed on 2021-10-22 11:57:30
